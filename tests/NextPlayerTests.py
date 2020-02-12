fdc265307f5a2f6d384a1b16b80c7fa0c1324ed4
Price <pricep@nevada.unr.edu>
2020-02-11 17:04:59 -0800

    Made NextPlayerTests.py

--- tests/NextPlayerTests.py
+++ tests/NextPlayerTests.py
@@ -1,9 +1,22 @@
 import unittest
+from GameDriver import GameDriver
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class CheckNextPlayerTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = GameDriver()
+
+    def test_O_player(self):
+        self.assertEqual(self.driver.current_player, 0)
+
+    def test_next_player(self):
+        self.driver.next_player()
+        self.assertEqual(self.driver.current_player, 1)
+
+    def test_1000_next_player(self):
+        for _ in range(100):
+            self.driver.next_player()
+        self.assertEqual(self.driver.current_player, 0)
 
 
 if __name__ == '__main__':
